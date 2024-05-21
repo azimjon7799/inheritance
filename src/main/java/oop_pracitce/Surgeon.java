b76478615f8a92fabcd132f6df6d694e690500c6
Abduazim Usmon <azimjonusmonov602@gmail.com>
2024-05-21 11:07:16 +0500

    Surgeon class ctreated

---
+++ src/main/java/oop_pracitce/Surgeon.java
@@ -0,0 +1,37 @@
+package oop_pracitce;
+
+public class Surgeon extends Doctor {
+    private int numberOfSurgeriesPerformed;
+
+    public Surgeon(String name, int age, String specialty, String licenseNumber, int numberOfSurgeriesPerformed) {
+        super(name, age, specialty, licenseNumber);
+        this.numberOfSurgeriesPerformed = numberOfSurgeriesPerformed;
+    }
+
+    public int getNumberOfSurgeriesPerformed() {
+        return numberOfSurgeriesPerformed;
+    }
+
+    public void setNumberOfSurgeriesPerformed(int numberOfSurgeriesPerformed) {
+        if (numberOfSurgeriesPerformed >= 0) {
+            this.numberOfSurgeriesPerformed = numberOfSurgeriesPerformed;
+        } else {
+            System.out.println("Number of surgeries performed must be non-negative.");
+        }
+    }
+
+    public void displayInfo(){
+        System.out.println("Surgeon name: " + getName());
+        System.out.println("Surgeon age: " + getAge());
+        System.out.println("Surgeon specialty: " + getSpecialty());
+        System.out.println("Surgeon license number: " + getLicenseNumber());
+        System.out.println("Number of surgeries performed: " + getNumberOfSurgeriesPerformed());
+    }
+
+    public static void main(String[] args) {
+        Surgeon surgeon = new Surgeon("Doctor D", 50, "Cardiothoracic Surgery", "ABC123456", 250);
+        surgeon.displayInfo();
+    }
+
+
+}
