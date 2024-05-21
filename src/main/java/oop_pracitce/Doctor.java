7cf6929ef656ff193bea8075bfd0109c2fa6bc87
Abduazim Usmon <azimjonusmonov602@gmail.com>
2024-05-21 11:03:22 +0500

    Some changes

--- src/main/java/oop_pracitce/Doctor.java
+++ src/main/java/oop_pracitce/Doctor.java
@@ -1,5 +1,52 @@
 package oop_pracitce;
 
 public class Doctor {
+    private String name;
+    private int age;
+    private String specialty;
+    private String licenseNumber;
 
+    public Doctor(String name, int age, String specialty, String licenseNumber) {
+        this.name = name;
+        this.age = age;
+        this.specialty = specialty;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Age must be positive.");
+        }
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
 }
+
